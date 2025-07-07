c07f060fa8c2658d2d892c410fb6bb224a8aa169
Alice Kim <alice.kim@qualia.com>
2025-07-06 20:53:22 -0700

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
